d4d7d45ff5f2b8a81fa9bf79811f15d1c891c541
David Benavidez <radicalralphhh@gmail.com>
2020-09-02 13:30:38 +0800

    b2c test

--- src/components/covid-feat-app-nav-ph.js
+++ src/components/covid-feat-app-nav-ph.js
@@ -25,13 +25,18 @@ export class AppNav extends LitElement {
     return navCss;
   }
 
+  connectedCallback() {
+    super.connectedCallback();
+
+  }
+
   render() {
     return html`
       <app-toolbar class="nav-container">
         ${(this.route === 'home') ? html`
           <paper-icon-button class="toggle-drawer" icon="menu" @click=${this.toggleDrawer} aria-label="Menu"></paper-icon-button>
         ` : ''}
-        
+
         <div main-title class="main-title">COVID-19 Tracker</div>
 
         <slot name="route-link"></slot>
@@ -40,20 +45,69 @@ export class AppNav extends LitElement {
           <paper-icon-button class="nav-container_button" aria-label="World Statistics" icon="language" @click=${this.showStatisticsModal}></paper-icon-button>
           <paper-icon-button class="nav-container_button" aria-label="Search Country" icon="search"></paper-icon-button>
           <paper-input class="nav-container_input" always-float-label label="Country Name" name="country" @value-changed=${this.searchCountryHandler}></paper-input>
-          
+
           <label for="sort-country-input">
             <select id="sort-country-input" @change=${this.updateSorter}>
-              <option value="" disabled selected hidden>Sort By</option>  
+              <option value="" disabled selected hidden>Sort By</option>
               <option value="Default">Default</option>
               <option value="Cases">Cases</option>
               <option value="Recovered">Recovered</option>
               <option value="Deaths">Deaths</option>
             </select>
           </label>
+
+          <button @click=${this.signIn}> Sign in </buttton>
         ` : ''}
-      
+
         </app-toolbar>
     `;
+  }
+  signIn() {
+  const b2cPolicies = {
+    names: {
+        signUpSignIn: "B2C_1_SignUpIn",
+        forgotPassword: "B2C_1_PasswordRest"
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: "https://ingcampaignbuilder.b2clogin.com/ingcampaignbuilder.onmicrosoft.com/B2C_1_SignUpIn/",
+        },
+        forgotPassword: {
+            authority: "https://ingcampaignbuilder.b2clogin.com/ingcampaignbuilder.onmicrosoft.com/B2C_1_PasswordRest",
+        },
+    },
+  }
+
+    const msalConfig = {
+      auth: {
+        clientId: "b62e15a3-f4be-466a-af1d-b11e1e3dcc47",
+        authority: b2cPolicies.authorities.signUpSignIn.authority,
+        validateAuthority: false,
+        redirectUri: "http://localhost:8000",
+      },
+      cache: {
+        cacheLocation: "localStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false // Set this to "true" to save cache in cookies to address trusted zones limitations in IE (see: https://github.com/AzureAD/microsoft-authentication-library-for-js/wiki/Known-issues-on-IE-and-Edge-Browser)
+      }
+    };
+    const myMSALObj = new Msal.UserAgentApplication(msalConfig);
+
+    /**
+     * Scopes you enter here will be consented once you authenticate. For a full list of available authentication parameters,
+     * visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_authenticationparameters_.html
+     */
+    const loginRequest = {
+      // scopes: ["openid", "profile"],
+    };
+
+    function signInFunc() {
+      myMSALObj.loginRedirect(loginRequest);
+    }
+
+    const result = signInFunc();
+
+    console.log('result');
+    console.log(result);
   }
 
   showStatisticsModal() {
